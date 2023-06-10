e33da1a2dce580fa1a75d5cc74ced8e555ff0921
ildar <ildar.faizov@gmail.com>
2023-06-10 23:38:07 +0400

    Bug: syslog pgdwn and pgup does not return to line 1

--- src/actions/pgup.rs
+++ src/actions/pgup.rs
@@ -1,5 +1,6 @@
 use cursive::event::{Event, EventResult, Key};
 use crate::actions::action::Action;
+use crate::model::cursor_shift::CursorShift;
 use crate::model::model::RootModel;
 
 pub struct PgUpAction {}
@@ -21,21 +22,7 @@ impl Action for PgUpAction {
 
     fn perform_action(&self, model: &mut RootModel, _event: &Event) -> EventResult {
         let h = model.get_viewport_height();
-        if model.scroll(-h) {
-            let p = model.data()
-                .and_then(|data| data.lines.first())
-                .map(|line| line.start);
-            if let Some(p) = p {
-                model.move_cursor_to_offset(p, false);
-            }
-        } else {
-            let p = model.data()
-                .and_then(|data| data.lines.first())
-                .map(|line| line.start);
-            if let Some(p) = p {
-                model.move_cursor_to_offset(p, false);
-            }
-        }
+        model.move_cursor(CursorShift::Y(-h), false);
         EventResult::Consumed(None)
     }
 }
