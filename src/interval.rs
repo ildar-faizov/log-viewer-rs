703b9d29deef4c9a1e0c3943447914b5bdb25709
ildar <ildar.faizov@gmail.com>
2023-08-05 19:19:05 +0400

    Interval intersection

--- src/interval.rs
+++ src/interval.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::{max, min, Ordering};
 use std::fmt;
 use std::fmt::{Display, Formatter};
 
@@ -12,7 +12,7 @@ pub enum IntervalBound<T> where T : Eq {
     }
 }
 
-#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Interval<T>
     where T: Ord, T: Eq, T: Copy {
     pub left_bound: IntervalBound<T>,
@@ -123,16 +123,29 @@ impl<T> Interval<T> where T: Ord, T: Eq, T: Copy {
         Self::builder().left_bound_inclusive(s).right_unbounded().build()
     }
 
+    pub fn inf_closed(s: T) -> Self {
+        Self::builder().left_unbounded().right_bound_inclusive(s).build()
+    }
+
+    pub fn open_inf(s: T) -> Self {
+        Self::builder().left_bound_exclusive(s).right_unbounded().build()
+    }
+
+    pub fn inf_open(s: T) -> Self {
+        Self::builder().left_unbounded().right_bound_exclusive(s).build()
+    }
+
     pub fn point(point: T) -> Self {
         Self::closed(point, point)
     }
 
-    pub fn contains_point(&self, value: &T) -> bool {
-        self.left_bound <= *value && self.right_bound >= *value
-    }
-
-    pub fn contains_interval(&self, _other: &Interval<T>) -> bool {
-        todo!()
+    pub fn intersect(&self, other: &Interval<T>) -> Interval<T> {
+        let left_bound = max(LeftBound(self.left_bound), LeftBound(other.left_bound)).0;
+        let right_bound = min(RightBound(self.right_bound), RightBound(other.right_bound)).0;
+        Interval {
+            left_bound,
+            right_bound
+        }
     }
 
     pub fn to_builder(self) -> IntervalBuilder<T> {
@@ -159,6 +172,94 @@ impl<T> Interval<T> where T: Ord, T: Eq, T: Copy {
                 }
             }
         }
+    }
+}
+
+impl<T> PartialEq<Self> for Interval<T> where T: Copy + Eq + Ord {
+    fn eq(&self, other: &Self) -> bool {
+        let empty1 = self.is_empty();
+        let empty2 = other.is_empty();
+        if !empty1 && !empty2 {
+            self.left_bound.eq(&other.left_bound)
+                && self.right_bound.eq(&other.right_bound)
+        } else {
+            empty1 == empty2
+        }
+    }
+}
+
+enum BoundSide {
+    Left,
+    Right
+}
+
+impl<T> IntervalBound<T> where T: Ord {
+    fn cmp(&self, other: &IntervalBound<T>, side: BoundSide) -> Ordering {
+        match &self {
+            IntervalBound::NegativeInfinity => {
+                match other {
+                    IntervalBound::NegativeInfinity => Ordering::Equal,
+                    _ => Ordering::Less,
+                }
+            },
+            IntervalBound::PositiveInfinity => {
+                match other {
+                    IntervalBound::PositiveInfinity => Ordering::Equal,
+                    _ => Ordering::Greater,
+                }
+            },
+            IntervalBound::Fixed { value: v1, is_included: i1 } => {
+                match other {
+                    IntervalBound::NegativeInfinity => Ordering::Greater,
+                    IntervalBound::PositiveInfinity => Ordering::Less,
+                    IntervalBound::Fixed { value: v2, is_included: i2 } => {
+                        match v1.cmp(v2) {
+                            Ordering::Equal => {
+                                if *i1 == *i2 {
+                                    Ordering::Equal
+                                } else {
+                                    match side {
+                                        BoundSide::Left => if *i1 { Ordering::Less } else { Ordering::Greater },
+                                        BoundSide::Right => if *i1 { Ordering::Greater } else { Ordering::Less },
+                                    }
+                                }
+                            },
+                            result => result
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+struct LeftBound<T>(IntervalBound<T>) where T: Ord + Eq + Copy;
+
+#[derive(Eq, PartialEq)]
+struct RightBound<T>(IntervalBound<T>) where T: Ord + Eq + Copy;
+
+impl<T> PartialOrd<Self> for LeftBound<T> where T: Ord + Eq + Copy {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.0.cmp(&other.0, BoundSide::Left))
+    }
+}
+
+impl<T> Ord for LeftBound<T> where T: Ord + Eq + Copy {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0, BoundSide::Left)
+    }
+}
+
+impl<T> PartialOrd<Self> for RightBound<T> where T: Ord + Eq + Copy {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.0.cmp(&other.0, BoundSide::Right))
+    }
+}
+
+impl<T> Ord for RightBound<T> where T: Ord + Eq + Copy {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0, BoundSide::Right)
     }
 }
 
--- src/interval_tests.rs
+++ src/interval_tests.rs
@@ -17,10 +17,10 @@ test_display!(1, Interval::closed(1, 2), "[1, 2]");
 test_display!(2, Interval::open_closed(-1, 2), "(-1, 2]");
 test_display!(3, Interval::closed_open(-1, 2), "[-1, 2)");
 test_display!(4, Interval::open(-3, 0), "(-3, 0)");
-test_display!(5, Interval::builder().left_unbounded().right_bound_inclusive(5).build(), "(-∞, 5]");
-test_display!(6, Interval::builder().left_unbounded().right_bound_exclusive(5).build(), "(-∞, 5)");
-test_display!(7, Interval::builder().left_bound_inclusive(5).right_unbounded().build(), "[5, +∞)");
-test_display!(8, Interval::builder().left_bound_exclusive(5).right_unbounded().build(), "(5, +∞)");
+test_display!(5, Interval::inf_closed(5), "(-∞, 5]");
+test_display!(6, Interval::inf_open(5), "(-∞, 5)");
+test_display!(7, Interval::closed_inf(5), "[5, +∞)");
+test_display!(8, Interval::open_inf(5), "(5, +∞)");
 test_display!(9, Interval::point(3), "[3, 3]");
 test_display!(10, Interval::<i32>::all(), "(-∞, +∞)");
 test_display!(11, Interval::<i32>::empty(), "∅");
@@ -49,3 +49,63 @@ test_empty!(2, Interval::closed(1, 1), "nonempty");
 test_empty!(3, Interval::<i32>::empty(), "empty");
 test_empty!(4, Interval::<i32>::all(), "nonempty");
 test_empty!(5, Interval::closed_inf(0), "nonempty");
+
+macro_rules! test_eq {
+    ($index: literal, $int1: expr, $int2: expr, "y") => {
+        paste! {
+            #[test]
+            fn [<test_eq_ $index>]() {
+                // assert_that!($int1.eq(&$int2)).is_true()
+                assert_that!($int1).is_equal_to(&$int2);
+            }
+        }
+    };
+    ($index: literal, $int1: expr, $int2: expr, "n") => {
+        paste! {
+            #[test]
+            fn [<test_ne_ $index>]() {
+                assert_that!($int1).is_not_equal_to(&$int2);
+            }
+        }
+    };
+}
+
+test_eq!("empty_empty", Interval::<i32>::empty(), Interval::<i32>::empty(), "y");
+test_eq!("empty_all", Interval::<i32>::empty(), Interval::<i32>::all(), "n");
+test_eq!("all_all", Interval::<i32>::all(), Interval::<i32>::all(), "y");
+test_eq!("empty_empty_non_normailized", Interval::<i32>::empty(), Interval::open(0, 0), "y");
+test_eq!("empty_non_normailized_empty_non_normailized",
+    Interval::closed(5, -1),
+    Interval::open(0, 0),
+    "y");
+test_eq!("finite_open", Interval::open(0, 1), Interval::open(0, 1), "y");
+test_eq!("finite_closed", Interval::closed(0, 1), Interval::closed(0, 1), "y");
+test_eq!("finite_open_finite_semi_open", Interval::open(0, 1), Interval::open_closed(0, 1), "n");
+test_eq!("finite_open_finite_closed", Interval::open(0, 1), Interval::closed(0, 1), "n");
+test_eq!("semi_finite_semi_finite", Interval::closed_inf(0), Interval::closed_inf(0), "y");
+
+macro_rules! test_intersection {
+    ($index: literal, $a: expr, $b: expr, $expected: expr) => {
+        paste! {
+            #[test]
+            fn [<test_intersect_ $index>]() {
+                let description = format!("{} * {} = {}", $a, $b, $expected);
+                asserting!(description).that(&$a.intersect(&$b)).is_equal_to(&$expected);
+            }
+
+            #[test]
+            fn [<test_intersect_ $index _reverse>]() {
+                let description = format!("{} * {} = {}", $b, $a, $expected);
+                asserting!(description).that(&$b.intersect(&$a)).is_equal_to(&$expected);
+            }
+        }
+    };
+}
+
+test_intersection!("all_all", Interval::<i32>::all(), Interval::<i32>::all(), Interval::<i32>::all());
+test_intersection!("all_empty", Interval::<i32>::all(), Interval::<i32>::empty(), Interval::<i32>::empty());
+test_intersection!("all_point", Interval::<i32>::all(), Interval::point(0), Interval::point(0));
+test_intersection!("all_finite", Interval::<i32>::all(), Interval::closed(0, 1), Interval::closed(0, 1));
+test_intersection!("oo_oo", Interval::open(0, 5), Interval::open(1, 10), Interval::open(1, 5));
+test_intersection!("oc_oo", Interval::open(0, 5), Interval::open_closed(1, 5), Interval::open(1, 5));
+test_intersection!("inf_open_open_inf", Interval::inf_open(5), Interval::open_inf(0), Interval::open(0, 5));
